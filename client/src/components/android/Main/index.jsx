5d262b1cccd9d7027d308c3e53de589c752d38cd
George A. Michel <george@nomadscience.org>
2021-07-24 01:31:13 -0700

    (feat) Add sprint burndown, PPD slider, colorized books to android/Main

--- client/src/components/android/Main/index.jsx
+++ client/src/components/android/Main/index.jsx
@@ -100,6 +100,21 @@ export default class Main extends Component {
     return weightedPomsLeft ? weightedPomsLeft + '/' + (weightedPomsLeft + pomsToDate) :
     `DONE (${pomsToDate} poms)`;
   }
+  @observable ppd = 8
+
+  @observable selectedTopicPPD = [0, 0, 0, 0]
+  @action setTopicPPD(value, index) {
+    this.selectedTopicPPD[index] = value;
+  }
+  @action setPPD(value) {
+    this.ppd = value;
+    this.selectedTopicPPD = this.selectedTopicPPD
+      .map(n => Math.min(value, n));
+  }
+  sumOtherPPD(index) {
+    return this.selectedTopicPPD
+      .reduce((acc, n, i) => index === i ? acc : acc + n, 0);
+  }
 
   get main() {
     const { common } = this.props;
@@ -111,14 +126,20 @@ export default class Main extends Component {
           `${pomsLeft ? '(' + Math.ceil(pomsLeft / ppd) + ' days)' : ''}` :
           '';
 
+    const colorFromBook = (book) => {
+      return getColorFromTask(book.tasks[book.tasks.length-1], common.parsleyData.lastUTC, 7);
+    };
+
     return pug`
       .screen
         h1.center Main
         p.center ${this.diegesisTitle}
         p.center ${this.blocksInScratchNotation}
         br
+        SprintBurnDown(legend=${true} common=${common} parsleyData=${common.parsleyData} ppd=${this.ppd} diegesis=${common.diegesis})
         p Poms Done Today: ${common.pomsToday}
-        p Saturation: <WIP>
+        // p Saturation: <WIP>
+        p Poms Per Day Target: ${this.ppd}
         br
         // h3 Active Topics
         // h4 <WIP>
@@ -129,16 +150,25 @@ export default class Main extends Component {
         //   )= topic
         // br
         h3 Active Books
-        div(style=${{ minHeight: 72, maxHeight: 370, overflow: 'auto', overflowX: 'hidden' }})
+        div(style=${{ minHeight: 72, maxHeight: 280, overflow: 'auto', overflowX: 'hidden' }})
           each book in this.activeBooks
-            div(key=book style={
-              display: 'flex',
-              height: 16,
-              whiteSpace: 'nowrap',
-            })
+            div(
+              key=book
+              style={
+                display: 'flex',
+                height: 20,
+                whiteSpace: 'nowrap',
+                background: 'red',
+                margin: 0,
+                alignItems: 'center',
+                border: '1px solid black',
+                borderBottom: 'unset',
+                background: colorFromBook(media[book]),
+              }
+              onClick=${e => media[book].goal && this.navTo(`/book_start`, { book })}
+            )
               h4(
                 key=book
-                onClick=${e => media[book].goal && this.navTo(`/book_start`, { book })}
                 style=${{
                   color: media[book].goal ? 'white' : '#aaa',
                 }}
@@ -149,16 +179,22 @@ export default class Main extends Component {
               h4(style={ width: (media[book].pomsLeft === 0 ? 20 : 90) })
                 = daysLeft(media[book])
         .flex-layer
-          .rows
-            h1 History
-            h1 Config
+          input.slider(
+            type="range"
+            min="0"
+            max="30"
+            value=${this.ppd}
+            onChange=${e => this.setPPD(+e.target.value)})
+          // .rows
+          //   h1 History
+          //   h1 Config
     `
   }
 
   get book_start() {
     if (!this.navState) {
       // Just temporarily, until I rejigger the rest; move to queryParams!
-      this.navState = { book: 'progphoenix' };
+      this.navState = { book: 'staffeng' };
     }
     // TODO: repattern to obviate this:
     const { parsleyData } = this.props.common;
@@ -266,7 +302,137 @@ export default class Main extends Component {
   render() {
     return this[this.path];
   }
+}
 
+function SprintBurnDown({ common, parsleyData, ppd, diegesis, legend }) {
+  if (!parsleyData) return null;
+  // assume they're just one task, split as needed, just as with books:
+  const day = diegesis.day; // note: this won't update appropriately
+  const binduColors = generateBinduColors(parsleyData, 'category');
+
+  const tasks = Array(day).fill().reduce((acc, _, offset) => {
+    const daysAgo = day-offset-1;
+    return acc.concat(common.tasksDaysAgo(daysAgo));
+  }, []);
+  const sprintGoal = ppd*2;
+
+
+  const splitTasks = [];
+  const cats = {};
+  tasks && tasks.forEach((task, i, a) => {
+    const duration = +task.duration;
+    const cat = task.category;
+    cats[cat] = !cats[cat] ? duration : cats[cat] + duration;
+    if (duration === 1) return splitTasks.push({ task, last: true, first: true });
+    Array(duration).fill().forEach((_, j) => {
+      splitTasks.push({ task, last: j === duration-1, first: j === 0});
+    });
+  });
+  const pomsLeft = Math.max(0, sprintGoal - splitTasks.length);
+
+  // Need to get poms for task; can move this out to somewhere else whenever:
+  let legendRef;
+
+  const adjustTransform = () => {
+    setTimeout(() => {
+      const $legend = document.querySelector('.sprint-burndown-component .legend')
+      if (!$legend) return;
+      if ($legend.clientWidth >= $legend.scrollWidth) return;
+      $legend.style.transform = `scale(${($legend.clientWidth/$legend.scrollWidth).toFixed(2)})`;
+    },0);
+  }
+
+  adjustTransform();
+  return pug`
+    .sprint-burndown-component(style=${{
+      width: '100%',
+    }})
+      .legend(
+        style=${{
+          marginTop: -10,
+          display: 'flex',
+          opacity: legend ? 1 : 0,
+          transformOrigin: '0% 100%',
+        }}
+      )
+        each cat, i in Object.entries(cats).sort((a, b) => a[1] - b[1])
+          .cat(
+            key=i
+            style=${{
+              fontSize: 10,
+              padding: '0px 0px',
+              border: '1px solid black',
+              margin: 2,
+              marginLeft: 0,
+              display: 'flex',
+              background: binduColors[cat[0]] || 'red',
+              filter: 'saturate(180%) hue-rotate(0deg)',
+            }}
+          )
+            .name(style=${{
+              width: '100%',
+              // paddingBottom: 1,
+              padding: '2px 0px',
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}) ${cat[0]}
+            .count(style=${{
+              borderLeft: '1px solid #0008',
+              width: 30,
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}) ${cat[1]}
+      .chart(style=${{
+          marginBottom: 10,
+          // marginTop: -10,
+          position: 'relative',
+          overflow: 'hidden',
+      }})
+        .percentage-container
+          each singlePom, i in splitTasks.slice(0, sprintGoal)
+              .bar-container(key=i style=${{
+                // margin: 0,
+                // marginRight: singlePom.last ? 1 : 0,
+                // opacity: i%2 ? 1 : 0.9,
+                filter: 'saturate(180%) hue-rotate(0deg)',
+              }})
+                .bar-top(
+                  style=${{
+                    height: (1 - i/sprintGoal)*100 + '%',
+                    background: binduColors[singlePom.task.category],
+                    opacity: 0.5,
+                  }}
+                )
+                .bar-body(
+                  style=${{
+                    height: (1 - (i+1)/sprintGoal)*100 + '%',
+                    background: binduColors[singlePom.task.category],
+                    writingMode: 'vertical-rl',
+                    textOrientation: 'upright',
+                    fontSize: 10,
+                    letterSpacing: -1,
+                    color: '#fff9',
+                    textAlign: 'right',
+                  }}
+                )
+                // ${!singlePom.last ? '' : singlePom.task.category[0]}
+          each _, i in Array(pomsLeft).fill()
+            .bar-forecast(
+              key=i
+              style=${{
+                height: (1 - (i + 1 + splitTasks.length)/sprintGoal)*100 + '%',
+              }}
+            )
+        .mid-bar(style=${{
+          position: 'absolute',
+          height: '100%',
+          width: '1px',
+          borderLeft: '1px dashed black',
+          left: '50%',
+        }})
+  `;
 }
 
 function BookBurnDown({
@@ -283,26 +449,6 @@ function BookBurnDown({
       splitTasks.push({ task, progress: pomProg });
     });
   });
-  const getColorFromTask = (splitTask) => {
-    const daysAgo = time.countDaysToDate(splitTask.task.baseDate, common.parsleyData.lastUTC);
-    switch (true) {
-      case (daysAgo === 0):
-        return '#f93f'; // orange
-      case (daysAgo >= -1):
-        return '#f3d'; // pink
-      case (daysAgo >= -7):
-        return '#b37'; // rose
-      case (daysAgo >= -30):
-        return '#75a'; // purplish
-      case (daysAgo >= -90):
-        return '#67a'; // cobalt blue
-      case (daysAgo >= -182):
-        return '#6a9'; // lighter teal
-      default:
-        return '#cba'; // beige
-    }
-
-  }
 
   if (!goal) return null;
   const weightedBarHeight = (i) =>
@@ -317,15 +463,14 @@ function BookBurnDown({
           .bar-container(key=i)
             .bar-top(
               style=${{
-                background: getColorFromTask(singlePom),
                 height: (!i ? 100 : (1 - splitTasks[i-1].progress/goal)*100) + '%',
-                background: getColorFromTask(singlePom),
+                background: getColorFromTask(singlePom.task, common.parsleyData.lastUTC),
               }}
             )
             .bar-body(
               style=${{
                 height: (1 - singlePom.progress/goal)*100 + '%',
-                background: getColorFromTask(singlePom),
+                background: getColorFromTask(singlePom.task, common.parsleyData.lastUTC),
               }}
             )
         each _, i in Array(weightedPomsLeft).fill()
@@ -336,4 +481,54 @@ function BookBurnDown({
             }}
           )
   `;
+}
+
+// FOR NOW! It's cool that this is Bindu Colors, RIIIIGHT???
+function generateBinduColors(parsley, property) {
+  var colors = {}
+  var stats = Object.keys(parsley.stats[property]);
+  var seed;
+
+  stats.forEach( function(stat) {
+    // sums the ascii values of each character in the stat to use as seed
+    var charSum = stat.split('').reduce( function(sum,item,i) { return sum + item.charCodeAt()*i+2 },0);
+    seed = charSum;
+
+    var color = {
+      r: parseInt(seededRandom()*100+50),
+      g: parseInt(seededRandom()*100+50),
+      b: parseInt(seededRandom()*100+100)
+    }
+    var colorString = "rgb("+color.r+','+color.g+','+color.b+")";
+
+    colors[stat] = colorString;
+
+    // $("#output").append('<div style="background:'+colorString+'">'+stat+' '+colorString+' '+charSum+'</div>');
+  });
+  return colors;
+
+  function seededRandom() {
+    var x = Math.sin(seed++) * 10000;
+    return x - Math.floor(x);
+  }
+}
+
+function getColorFromTask(task, nowUTC, hexAlpha = '') {
+  const daysAgo = time.countDaysToDate(task.baseDate, nowUTC);
+  switch (true) {
+    case (daysAgo === 0):
+      return '#f93' + hexAlpha; // orange
+    case (daysAgo >= -1):
+      return '#f3d' + hexAlpha; // pink
+    case (daysAgo >= -7):
+      return '#b37' + hexAlpha; // rose
+    case (daysAgo >= -30):
+      return '#75a' + hexAlpha; // purplish
+    case (daysAgo >= -90):
+      return '#67a' + hexAlpha; // cobalt blue
+    case (daysAgo >= -182):
+      return '#6a9' + hexAlpha; // lighter teal
+    default:
+      return '#cba' + hexAlpha; // beige
+  }
 }
